c5e8a46eda914610e42a4b90208560109c2adbe1
Galastrod <albertmagnus829@gmail.com>
2023-11-23 18:03:31 +0300

    Fix prodject starting

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import uvicorn
-from app import app
+from os import getenv
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8181)
+if __name__ == "__main__" :
+	port = int( getenv( "PORT", 8000 ) );
+	uvicorn.run( "app:app", host="0.0.0.0", port=port, reload=True)
 
 

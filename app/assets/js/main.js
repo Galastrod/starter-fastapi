15af8e1f0a9df7831a8069f440c063d446f70f9b
Galastrod <albertmagnus829@gmail.com>
2023-11-23 19:47:55 +0300

    New books list

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -14,16 +14,28 @@ const app = new Vue({
 	`
 		<div class="app container">
 			<div class="b-search">
-				<h2>Введите название книги...</h2>
-				<div class="b-search-inp">
-					<input class="search__inp" placeholder="Введите название книги.." v-model="search_string">
-					<button class="search__btn" @click="search">Искать</button>
+				<div class="search-header">
+					<h2>Введите название книги...</h2>
+					<div class="b-search-inp">
+						<input class="search__inp" placeholder="Введите название книги.." v-model="search_string">
+						<button class="search__btn" @click="search">Искать</button>
+					</div>
 				</div>
 				
 				<div class="b-books-list">
 					<h2>{{book_list_title}}</h2>
 					<div v-for="book in books_list">
-						<h3>{{ book.title }}</h3>
+						<div>
+							<h3>{{ book.title }}</h3>
+							<a :href="book.links.author_link">{{book.author}}</a>
+							<a v-if="book.links.sequence_link" :href="book.links.sequence_link">Все книги серии</a>
+						</div>
+						<div>
+							<h3>Скачать: </h3>
+							<a v-if="book.links.download_fb2" :href="book.links.download_fb2">Fb2 </a>
+							<a v-if="book.links.download_epub" :href="book.links.download_epub">Fb2 </a>
+							<a v-if="book.links.download_mobi" :href="book.links.download_mobi">Fb2 </a>
+						</div>
 					</div>
 				</div
 			</div>
